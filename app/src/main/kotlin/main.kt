fb6e28c5672ee8a72380d62b73bb843c8d5da978
azul915 <k.aoyagi940516@gmail.com>
2022-05-06 20:34:18 +0900

    update

--- app/src/main/kotlin/main.kt
+++ app/src/main/kotlin/main.kt
@@ -1,41 +1,28 @@
 package main
 
+import twitter4j.Status
 import twitter4j.Twitter
-import twitter4j.conf.Configuration
 import twitter4j.conf.ConfigurationBuilder
 import twitter4j.TwitterFactory
 
 fun main(args: Array<String>) {
 
-    val configuration: Configuration = ConfigurationBuilder().run {
-        val env = Env()
-        this.setOAuthConsumerKey(env.OAuthConsumerKey)
-        this.setOAuthConsumerSecret(env.OAuthConsumerSecret)
-        this.setOAuthAccessToken(env.OAuthAccessToken)
-        this.setOAuthAccessTokenSecret(env.OAuthAccessTokenSecret)
-        this.build()
-    }
-    val twitter = TwitterFactory(configuration).instance
+    val twitter: Twitter = TwitterFactory(
+            ConfigurationBuilder().run {
+                this.setOAuthConsumerKey(System.getenv("CONSUMER_KEY"))
+                this.setOAuthConsumerSecret(System.getenv("CONSUMER_SECRET"))
+                this.setOAuthAccessToken(System.getenv("ACCESS_TOKEN"))
+                this.setOAuthAccessTokenSecret(System.getenv("ACCESS_TOKEN_SECRET"))
+                this.build()
+            }
+    ).instance
 
-//    // show timeline
-//    twitter.getHomeTimeline().forEach {
+    // show timeline
+//    twitter.homeTimeline.forEach {
 //        println("${it.user.name} : ${it.text}")
 //    }
 
     // tweet test
-    val status = twitter.updateStatus("test")
+    val status: Status = twitter.updateStatus("test")
     println(status.text)
-}
-
-class Env() {
-    val OAuthConsumerKey: String
-    val OAuthConsumerSecret: String
-    val OAuthAccessToken: String
-    val OAuthAccessTokenSecret: String
-    init {
-        OAuthConsumerKey = System.getenv("CONSUMER_KEY")
-        OAuthConsumerSecret = System.getenv("CONSUMER_SECRET")
-        OAuthAccessToken = System.getenv("ACCESS_TOKEN")
-        OAuthAccessTokenSecret = System.getenv("ACCESS_TOKEN_SECRET")
-    }
 }
